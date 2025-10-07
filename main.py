4aaab668b569d942194d32e95f17b9db3cdf8adb
Matthew Gregory <mammykins1337@gmail.com>
2025-10-07 22:19:45 +0800

    Refactor count_words into stats.py

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
+from stats import count_words
+
+
 def get_book_text(path_to_file):
     """It takes a file path as input and returns the contents of the file as a string"""
     with open(path_to_file) as f:
         file_contents = f.read()
         return file_contents
-
-
-def count_words(text):
-    "Counts the number of words in a string"
-    text = text.split()
-    return len(text)
 
 
 def main():
---
+++ stats.py
@@ -0,0 +1,4 @@
+def count_words(text):
+    "Counts the number of words in a string"
+    text = text.split()
+    return len(text)
